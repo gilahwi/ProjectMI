59b6763f66291e7ef825ec360f0a11c015cc2b69
gilahwi <gilahwi@users.noreply.github.com>
2018-11-01 17:20:35 +0200

    #2 cleaned code

--- cheermeup.py
+++ cheermeup.py
@@ -1,13 +1,17 @@
 from mynltk.newstuff import cheer
 
+
 def how_many_times():
     cheer()
     cheer()
     cheer()
 
+
 how_many_times()
+
 
 def doing_good():
     print('Keep up the good work!')
+
 
 doing_good()
